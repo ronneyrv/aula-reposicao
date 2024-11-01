a868cee675ab791e32319ee250c42de098137b49
RAIMUNDO RONNEY DA ROCHA VIEIRA <RAIMUNDO0339@DIGITALCOLLEGE.LOCAL>
2024-11-01 18:23:50 -0300

    exercicio 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt('Insira um número:'));
+
+if (numero > 0) {
+    alert(`O número ${numero} é positivo!`);
+} else if (numero === 0) {
+    alert(`O número ${numero} é igual a ZERO`);
+} else {
+    alert(`O número ${numero} é negativo!`)
+}
