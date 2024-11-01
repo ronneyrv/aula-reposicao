3a543ae1d21f94ac9f8748686cd8c614328dad8f
RAIMUNDO RONNEY DA ROCHA VIEIRA <RAIMUNDO0339@DIGITALCOLLEGE.LOCAL>
2024-11-01 19:07:55 -0300

    exerciocio 4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-let numero = parseFloat(prompt('Insira um número:'));
+function exercicio1() {
+    let numero_input = prompt('Insira um número:');
 
-if (typeof(numero) == "number" && !Number.isNaN(numero)) {
+    let numero = parseFloat(numero_input);
 
-    if (numero > 0) {
-        alert(`O número ${numero} é positivo!`);
-    } else if (numero === 0) {
-        alert(`O número ${numero} é igual a ZERO`);
+    if (numero_input.length === String(numero).length) {
+        if (typeof (numero) == "number" && !Number.isNaN(numero)) {
+
+            if (numero > 0) {
+                alert(`O número ${numero} é positivo!`);
+            } else if (numero === 0) {
+                alert(`O número é igual a ZERO`);
+            } else {
+                alert(`O número ${numero} é negativo!`)
+            }
+
+        } else {
+            alert("Insira um número válido.")
+        }
     } else {
-        alert(`O número ${numero} é negativo!`)
+        alert("Insira um número válido.")
     }
-    
-} else {
-    alert("Insira um número válido")
 }
+
+function exercicio4() {
+    `
+    Questão 4: Escreva um script que verifique se um número é par ou ímpar (utilize o operador %) 
+    e exiba uma mensagem informando o resultado da verificação:
+    Possíveis mensagens: "O número xx é par", "O número xx é ímpar"
+    `
+    let numero = prompt('Insira um número:');
+
+    numero % 2 == 1 ? alert('Número impar') : alert('Número par');
+
+}
+
+exercicio4();
